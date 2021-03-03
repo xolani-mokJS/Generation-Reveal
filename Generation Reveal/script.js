6f8dca3ba84f1e98cfffde894356326d6177f280
Monde <monde.ngalo@gmail.com>
2021-03-03 22:14:19 +0200

    Implement JS logic to print to document

--- Generation Reveal/script.js
+++ Generation Reveal/script.js
@@ -1,24 +1,52 @@
 const form = document.getElementById("my-form");
+const outputWrapper = document.querySelector('.output-text');
 
-form.addEventListener("submit", function genReveal(event){
-    event.preventDefault();
-    
-    const yearOfBirth = document.getElementById("year-of-birth").value;
-    console.log (yearOfBirth);
-
-    if (yearOfBirth >= 1928 && yearOfBirth <= 1945){
-        console.log('silent');
-    } else if(yearOfBirth >= 1946 && yearOfBirth <= 1964){
-        console.log('Boomer');
-    }else if(yearOfBirth >= 1965 && yearOfBirth <= 1980){
-        console.log('GenZ');
-    }else if(yearOfBirth >= 1981 && yearOfBirth <= 1997){
-        console.log('Millenial');
-    }else if( yearOfBirth >= 1998 && yearOfBirth <= 2012){
-        console.log('GenY');
-    }else {
-        console.log('still working');
+/**
+ * Another way of defining a function (called arrow function or something)
+ *  the function is the `(var)=> {}` part. But here we assign it to a constant
+ *
+ * Source: https://edition.cnn.com/2013/11/06/us/baby-boomer-generation-fast-facts/index.html
+ */
+const calcGen = (yob) => {
+    if (yob <= 1924) {
+        return 'GI'; // Or, the Great Generation
+    } else if (yob <= 1945) { // I think it's anytime upto 1945?
+        return 'silent';
+    } else if (yob <= 1964) { // we don't need to mention the lower year, since the 'if' above handles it...
+        return 'Boomer';
+    } else if (yob <= 1980) {
+        return 'GenX';
+    } else if (yob <= 1996) {
+        return 'Millenial'; // aka Gen Y
+    } else {
+        return 'GenZ';
     }
+}
 
+const displayGen1 = function(gen) {
+    let ot = document.getElementById('output-text');
+    // Prefixing with 'you're a -' doesn't always make sense (eg, for You're a silent)
+    //   I just did it to show string templates.
+    ot.innerText = `You're a ${gen}!`;
+}
+
+/**
+ * You could also generate the <p> element when you need it,
+ *  so that you don't have an empty <p> lying around
+ */
+function displayGen2(gen) {
+    // Remove any children. In case a <p> was already printed.
+    outputWrapper.innerHTML = '';
+    let ot = document.createElement('P');
+    ot.id = 'output-text';
+    ot.innerText = gen;
+    outputWrapper.appendChild(ot);
+}
+
+// If you're not gonna use a function's name, then you don't need to give it a name.
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const yearOfBirth = document.getElementById("year-of-birth").value;
+    console.log(yearOfBirth);
+    displayGen2(calcGen(yearOfBirth));
 })
-
